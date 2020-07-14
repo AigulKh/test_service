e7b1596463fff5f42f93fe5435a68fc9ba1d1177
AigulKh <aigul.kh017@gmail.com>
2020-07-14 03:11:37 +0300

    basic classes

--- src/User.kt
+++ src/User.kt
@@ -21,40 +21,24 @@ class User(val login: String, var password: String, var count_present: Int = 0) 
     fun getIncomingRequests(db: MongoDataService) = db.getIncomingRequests(this.login)
 
     fun sendFriendRequest(user_login: String, db: MongoDataService) {
-        if (getFriends(db).contains(user_login) || db.checkRequest(this.login, user_login) || db.checkRequest(user_login, this.login)) {
-            println("Заявка ${this.login} -> ${user_login} не может быть создана;")
-            return
-        }
+        if (getFriends(db).contains(user_login) || db.checkRequest(this.login, user_login) || db.checkRequest(user_login, this.login)) return
         Request(this.login, user_login).addRequest(db)
         db.sendFriendRequest(this.login, user_login)
-        println("Заявка ${this.login} -> ${user_login} создана;")
     }
 
     fun acceptFriendRequest(request: Request, db: MongoDataService) {
-        if (!db.checkRequest(request.sender_login, request.recipient_login)) {
-            println("Заявка ${request.sender_login} -> ${request.recipient_login} не может быть принята;")
-            return
-        }
+        if (!db.checkRequest(request.sender_login, request.recipient_login)) return
         request.accept(db)
-        println("Заявка ${request.sender_login} -> ${request.recipient_login} принята;")
     }
 
     fun rejectFriendRequest(request: Request, db: MongoDataService) {
-        if (!db.checkRequest(request.sender_login, request.recipient_login)) {
-            println("Заявка ${request.sender_login} -> ${request.recipient_login} не может быть отклонена;")
-            return
-        }
+        if (!db.checkRequest(request.sender_login, request.recipient_login)) return
         request.reject(db)
-        println("Заявка ${request.sender_login} -> ${request.recipient_login} отклонена;")
     }
 
     fun deleteFriend(user_login: String, db: MongoDataService) {
-        if (!getFriends(db).contains(user_login)) {
-            println("Пользователь ${this.login} не может быть удалён из друзей ${user_login};")
-            return
-        }
+        if (!getFriends(db).contains(user_login)) return
         db.deleteFriend(this.login, user_login)
-        println("Пользователь ${this.login} удалил ${user_login} из друзей;")
     }
 
     fun getSentAcceptedPresents(db: MongoDataService) = db.getSentPresents(this.login, true)
@@ -74,22 +58,14 @@ class User(val login: String, var password: String, var count_present: Int = 0) 
     }
 
     fun sendPresent(user_login: String, db: MongoDataService) {
-        if (!getFriends(db).contains(user_login) || this.count_present > N || checkUnacceptedPresent(user_login, db)) {
-            println("Пользователь ${this.login} не может отправить подарок ${user_login};")
-            return
-        }
+        if (!getFriends(db).contains(user_login) || this.count_present > N || checkUnacceptedPresent(user_login, db)) return
 
         Present("${this.login}_${LocalDateTime.now()}_$count_present", this.login, user_login).addPresent(db)
         this.count_present++
-        println("Пользователь ${this.login} отправил подарок ${user_login};")
     }
 
     fun acceptPrecent(present: Present, db: MongoDataService) {
-        if (!db.checkPresent(present)) {
-            println("Подарок ${present.sender_login} -> ${present.recipient_login} не может быть принят;")
-            return
-        }
+        if (!db.checkPresent(present)) return
         present.accept(db)
-        println("Подарок ${present.sender_login} -> ${present.recipient_login} принят;")
     }
 }
